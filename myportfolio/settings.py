3197d4fb3e9432f1dd3ce2b379ba8acd8b46aafc
daryldewilde <nfoyedewilde@gmail.com>
2024-11-07 22:45:26 +0100

    allowed all hosts

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%9rn93r_j-hx^_o=*%_=lyhlavtbefi(^7pe==so44z%6l$9_u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
